5ac105e4c170507aaaed5e9fbc65f7709124f801
wangjh <2206222540@qq.com>
2022-07-23 11:16:02 +0800

    【补充设置界面脚本】

---
+++ Assets/Scripts/Resources/UI/Common/SettingsWindow.cs
@@ -0,0 +1,37 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System.Threading.Tasks;
+using TMPro;
+using BXB.Core;
+
+public class SettingsWindow : MiUIDialog
+{
+    public Image image;
+    [SerializeField] MiUIButton CloseButton;
+    protected override async Task OnAwakeAsync()
+    {
+        await base.OnAwakeAsync();
+
+        CloseButton.onClick.SubscribeEventAsync(async () =>
+        {
+           gameObject.SetActive(false);
+           image.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        });
+
+    }
+    public override void OnInit()
+    {
+        gameObject.SetActive(true);
+        image.color = new Color(0.0f, 0.0f, 0.0f, 0.88f);
+    }
+    public override void OnSetInit(object[] value)
+    {
+
+    }
+    public override async Task OnSetInitAsync<T>(params object[] value)
+    {
+        await AsyncDefaule();
+    }
+}
