601b5c89bfdc5a67b366331cc321f17dd79dd1d1
hjh <1935095524@qq.com>
2022-07-23 20:15:06 +0800

    q

---
+++ Assets/Scripts/Test/NewBehaviourScript1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace BXB
+{
+    namespace Core
+    {
+
+        public class NewBehaviourScript1 : MonoBehaviour
+        {
+
+            // Use this for initialization
+            void Start()
+            {
+
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+
+            }
+        }
+    }
+}
