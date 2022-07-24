11a98df63407e2182d9a2522423f552a6a097b86
wangjh <2206222540@qq.com>
2022-07-24 21:14:47 +0800

    【音效+招募】

--- Assets/Scripts/SoundManager/SoundManager.cs
+++ Assets/Scripts/SoundManager/SoundManager.cs
@@ -9,10 +9,22 @@ public class SoundManager : MonoBehaviour
     [SerializeField]
     private AudioSource audioBGMSource;
 
+
     [SerializeField]
     private AudioSource audioSESource;
     [SerializeField]
+    private AudioClip globalBGMSound;
+    [SerializeField]
     private AudioClip playerWalkSound; /*playerAttackSound;*/
+    [SerializeField]
+    private AudioClip playerAttackSound;
+    [SerializeField]
+    private AudioClip BossAttackSound;
+    [SerializeField]
+    private AudioClip EnemyAttackSound;
+    [SerializeField]
+    private AudioClip BossBgmSound;
+
     private void Awake()
     {
         instance = this;
@@ -21,24 +33,47 @@ public class SoundManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioBGMSource.clip = globalBGMSound;
+        audioBGMSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void PlayerWalkAudio() 
+    public void PlayerWalkAudio()
     {
         audioSESource.clip = playerWalkSound;
         audioSESource.Play();
     }
 
-   /* public void PlayerAttackAudio()
+    public void PlayerAttackAudio()
     {
-        audioSource.clip = playerAttackSound;
-        audioSource.Play();
-    }*/
+        audioSESource.clip = playerAttackSound;
+        audioSESource.Play();
+    }
+
+    public void BossAttackAudio()
+    {
+        audioSESource.clip = BossAttackSound;
+        audioSESource.Play();
+    }
+
+    public void EnemyAttackAudio()
+    {
+        audioSESource.clip = EnemyAttackSound;
+        audioSESource.Play();
+    }
+
+    public void EntryBossEnviroment()
+    {
+        if (audioBGMSource.isPlaying)
+        {
+            audioBGMSource.Stop();
+        }
+        audioBGMSource.clip = BossBgmSound;
+        audioBGMSource.Play();
+    }
 }
---
+++ Assets/SoundLogic.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayerWalkPlaySound()
+    {
+        SoundManager.instance.PlayerWalkAudio();
+    }
+
+    public void OnPlayerAttackPlaySound()
+    {
+        SoundManager.instance.PlayerAttackAudio();
+    }
+
+    public void OnBossAttackPlaySound()
+    {
+        SoundManager.instance.BossAttackAudio();
+    }
+
+}
